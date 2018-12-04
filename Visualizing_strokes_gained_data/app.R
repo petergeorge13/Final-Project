0c43cd63408cc7dc87fb00af6fd1a9889318ca45
Peter George <petergeorge@college.harvard.edu>
2018-12-04 17:03:07 -0500

    Final Commit for Real

--- Visualizing_strokes_gained_data/app.R
+++ Visualizing_strokes_gained_data/app.R
@@ -20,13 +20,15 @@ ui <- fluidPage(
   
   titlePanel("Strokes Gained by Round and Player in Various Tournaments"),
   
-  ## Creating a sidebar with one panel icluding six input dropdowns and six segments of text. I wanted to make sure that each graph would be interactive and that there was enough explination for each of them for anyone in the future to be able to come in and use the app.
-  ## I'm creating multiple inputs for both rounds and players because I want the user to be able to select over what rounds and which players they want the total strokes gained data to show up for.
-  ## This will allow the user to compare multiple players over multiple rounds to see who performed better or worse by round
-  ## I left the defaults as round one and Mickelson because tournaments start with the first round and most people recognize the name Phil Mickelson more than most other professional golfers, other than Tiger Woods. Unforunately Tiger did not play in the WGC-Mexico tournament and I wanted all of the defaults to be the same.
+  ## Creating a sidebar with one panel icluding six input dropdowns and six segments of text. I wanted to make sure that each graph would be interactive and that there was enough explination for each of them for anyone in the future to be able to come in and use the app
   
   sidebarLayout(
     sidebarPanel(
+      
+      ## I'm creating multiple inputs for both rounds and players because I want the user to be able to select over what rounds and which players they want the total strokes gained data to show up for.
+      ## This will allow the user to compare multiple players over multiple rounds to see who performed better or worse by round
+      ## I left the defaults as round one and Mickelson because tournaments start with the first round and most people recognize the name Phil Mickelson more than most other professional golfers, other than Tiger Woods. Unforunately Tiger did not play in the WGC-Mexico tournament and I wanted all of the defaults to be the same.
+      
       selectInput("round",
                   "Round Number - Players",
                   choices = x$round,
@@ -44,6 +46,10 @@ ui <- fluidPage(
       
       h6("Select the player(s) for which strokes gained by round data will be shown for The Players Championship"),
       
+      ## I'm creating multiple inputs for both rounds and players because I want the user to be able to select over what rounds and which players they want the total strokes gained data to show up for.
+      ## This will allow the user to compare multiple players over multiple rounds to see who performed better or worse by round
+      ## I left the defaults as round one and Mickelson because tournaments start with the first round and most people recognize the name Phil Mickelson more than most other professional golfers, other than Tiger Woods. Unforunately Tiger did not play in the WGC-Mexico tournament and I wanted all of the defaults to be the same.
+      
       selectInput("round2",
                   "Round Number - Memorial",
                   choices = y$round,
@@ -60,6 +66,10 @@ ui <- fluidPage(
                   selected = "Mickelson"),
       
       h6("Select the player(s) for which strokes gained by round data will be shown for The Memorial Tournament"),
+      
+      ## I'm creating multiple inputs for both rounds and players because I want the user to be able to select over what rounds and which players they want the total strokes gained data to show up for.
+      ## This will allow the user to compare multiple players over multiple rounds to see who performed better or worse by round
+      ## I left the defaults as round one and Mickelson because tournaments start with the first round and most people recognize the name Phil Mickelson more than most other professional golfers, other than Tiger Woods. Unforunately Tiger did not play in the WGC-Mexico tournament and I wanted all of the defaults to be the same.
       
       selectInput("round3",
                   "Round Number - WGC-Mexico",
@@ -79,17 +89,22 @@ ui <- fluidPage(
       h6("Select the player(s) for which strokes gained by round data will be shown for WGC- Mexico")),
     
     ## I created a main panel with four different tabs, the first being an explination of what is going on in the app and than the next three being interactive graphs for three different tournaments that can be adjusted by using the sidebars corresponsing to that tournament.
-    ## My explanation details how strokes gained are used and how the app can be used to compare players by roudn and by tournament.
-    ## The three graphs are showing the same data points but across different tournaments. I wanted to show the method that I used is replicable across tournaments with this data and that players strokes gained can vary across tournaments. FOr example, Mickelson had overall poor strokes gained in the Players and Memorial tournaments so the field was generally better than him, where in the WGC-Mexico he had positive strokes gained in each round and eventually won the tournament.
     
     mainPanel(
       tabsetPanel(type = "tabs",
+                  
+                  ## My explanation details how strokes gained are used and how the app can be used to compare players by round and by tournament.
+                  
                   tabPanel("Explanation", h4("This app is designed to analyze strokes gained data across tournaments, players and rounds. Strokes gained is a statistical metric to determine how well a golfer is doing in relation to another set of golfers shot by shot. 
                                              A positive strokes gained means that the player did better than the field on that shot or in that round. The opposite is true of a negative strokes gained. In this app, the strokes gained represented are in strokes gained 
                                              per round, which is just the total strokes gained per shot in the round all added up. The other three panels represent three tournaments played on the PGA Tour in the 2018 season: The Players Championship, The Memorial Tournament, 
                                              and The World Golf Championship - Mexico. For each graph there are two input selectors, round and player last name. These can be adjusted to compare multiple golfers and multiple rounds within the same tournament. 
                                              By comparing strokes gained across rounds we can predict how well one player did in comparison to another, hypothesizing the order they placed in the tournament. If strokes gained is an accurate measure, the comparison of any number of players should be correct.
                                              This data came from the PGA Tour Shotlink Intelligence System." )),
+                  
+                  ## The three graphs are showing the same data points but across different tournaments. I wanted to show the method that I used is replicable across tournaments with this data and that players strokes gained can vary across tournaments. FOr example, Mickelson had overall poor strokes gained in the Players and Memorial tournaments so the field was generally better than him, 
+                  ## where in the WGC-Mexico he had positive strokes gained in each round and eventually won the tournament.
+                  
                   tabPanel("Players", plotOutput("plot")),
                   tabPanel("Memorial", plotOutput("plot2")),
                   tabPanel("WGC-Mexico", plotOutput("plot3"))
